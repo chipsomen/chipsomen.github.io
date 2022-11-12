447ea0931f534556250d4183b458af1fe0e07a5a
chipsomen <112735090+chipsomen@users.noreply.github.com>
2022-11-11 20:49:25 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,44 @@
+function openCity(evt, cityName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
+
+function openLink(evt, animName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(animName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
+
+// function by chip
+var current = 0
+function changeBackground(){
+  var body, bgList;
+  bgList = [
+    "url(art/mali.jpg)",
+    "url(art/dance.jpg)",
+    "url(art/islam.jpg)",
+    "url(art/musa.jpg)",
+    "url(art/imposter.jpg)"
+  ];
+  body = document.body.style;
+  current++
+  current = current % bgList.length;
+  body.backgroundImage = bgList[current]
+}
